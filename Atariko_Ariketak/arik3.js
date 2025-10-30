da9bcfc4559ee060e2ad69437b24ae8111224843
Maddi <maguinaga005@ikasle.ehu.eus>
2025-10-30 10:18:30 +0100

    arik3.js eginda

---
+++ Atariko_Ariketak/arik3.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+
+// Datuak lortu, competitions.json fitxategian gorde string moduan eta ESP, ENG, ITA, FRA herrialdeetako TIER_ONE ligak lortu
+fetch('http://api.football-data.org/v4/competitions')
+    .then(
+        r => r.json())
+    .then(
+        data => {
+            // Lortutako json-a competitions.json fitxategian gorde string moduan
+            fs.writeFileSync('competitions.json', JSON.stringify(data, null, 2));
+            console.log('competitions.json fitxategia gordeta.');
+
+            // 'plan = TIER_ONE' duten objektuak bilatu
+            const competitions_TIER_ONE = data.competitions.filter(c => c.plan === "TIER_ONE");
+
+            // ESP, ENG, ITA, FRA herrialdeetakoak lortu
+            const competitions_TIER_ONE_ESP_ENG_ITA_FRA = competitions_TIER_ONE.filter(c => c.area.name === "Spain" || c.area.name === "Italy" || c.area.name === "England" || c.area.name === "France");
+
+            console.log('ESP, ENG, ITA, FRA herrialdeetako TIER_ONE ligak lortu:');
+            console.log(competitions_TIER_ONE_ESP_ENG_ITA_FRA);
+        }
+    )
+
+
