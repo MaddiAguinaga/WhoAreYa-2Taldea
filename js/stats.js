ad351ba20afaeebf273dedd25c0a0ea8427d261e
mfernandez429 <mfernandez429@ikasle.ehu.eus>
2025-11-14 12:45:49 +0100

    initState-funtzioa done

--- js/stats.js
+++ js/stats.js
@@ -3,6 +3,31 @@ export {initState}
 let initState = function(what, solutionId) {
 
     // YOUR CODE HERE
+
+    // localstorage-etik lortu objektua
+    let obj = localStorage.getItem(what);
+    let state;
+
+    if (obj) { // objektua existitzen bada, parseatu
+        state = JSON.parse(obj);
+    } else { // objektua ez bada existitzen, sortu objektu berria guesses hutsik jarriz
+        state = {
+            solutionId: solutionId,
+            guesses: []
+        };
+
+        // localStorage-en gorde
+        localStorage.setItem(what, JSON.stringify(state));
+    }
+
+    // Guess berriak gorde eta localStorage-n eguneratzeko funtzioa
+    let addGuess = function (guess) {
+        state.guesses.push(guess);          // guess gehitu state-ra
+        localStorage.setItem(what, JSON.stringify(state));  // localStorage eguneratu
+    };
+
+    // Bi baloreko array-a bueltatu [state, funtzioa]
+    return [state, addGuess];
 }
 
 
