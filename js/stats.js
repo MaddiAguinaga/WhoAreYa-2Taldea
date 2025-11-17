8bb0d29290f5975a08a49659e07fc8ecfe441c62
Maddi <maguinaga005@ikasle.ehu.eus>
2025-11-17 17:42:03 +0100

    addGuess() funtzioan modifikazioa

--- js/stats.js
+++ js/stats.js
@@ -20,7 +20,7 @@ let initState = function(what, solutionId) {
 
     // Guess berriak gorde eta localStorage-n eguneratzeko funtzioa
     let addGuess = function (guess) {
-        state.guesses.push(guess);          // guess gehitu state-ra
+        // state.guesses.push(guess);          // guess gehitu state-ra   | komentatu ta bestela jolastean 2 guess gehitzen dira
         localStorage.setItem(what, JSON.stringify(state));  // localStorage eguneratu
     };
 
